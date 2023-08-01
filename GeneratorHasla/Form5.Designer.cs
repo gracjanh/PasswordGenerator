953d6113ef00dd9373d0ebd1697eb1e4a9a2747c
gracjanh <74767350+gracjanh@users.noreply.github.com>
2023-08-01 17:24:16 +0200

    Add files via upload

---
+++ GeneratorHasla/Form5.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace GeneratorHasla
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form5));
+            panel1 = new Panel();
+            label3 = new Label();
+            button10 = new Button();
+            button9 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            panel2 = new Panel();
+            button7 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            textBox1 = new TextBox();
+            panel3 = new Panel();
+            button8 = new Button();
+            button5 = new Button();
+            textBox4 = new TextBox();
+            button6 = new Button();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(button10);
+            panel1.Controls.Add(button9);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(listBox1);
+            panel1.Location = new Point(0, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(984, 546);
+            panel1.TabIndex = 0;
+            panel1.Visible = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(61, 65);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 25);
+            label3.TabIndex = 56;
+            label3.Text = "Hasła:";
+            // 
+            // button10
+            // 
+            button10.BackColor = SystemColors.Highlight;
+            button10.Cursor = Cursors.Hand;
+            button10.FlatStyle = FlatStyle.Flat;
+            button10.Font = new Font("Microsoft Sans Serif", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            button10.ForeColor = Color.White;
+            button10.Location = new Point(262, 479);
+            button10.Name = "button10";
+            button10.Size = new Size(119, 50);
+            button10.TabIndex = 42;
+            button10.Text = "Usuń hasło";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.Highlight;
+            button9.Cursor = Cursors.Hand;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button9.ForeColor = Color.White;
+            button9.Location = new Point(387, 479);
+            button9.Name = "button9";
+            button9.Size = new Size(119, 50);
+            button9.TabIndex = 41;
+            button9.Text = "Wyloguj";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.Highlight;
+            button2.Cursor = Cursors.Hand;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(137, 479);
+            button2.Name = "button2";
+            button2.Size = new Size(119, 50);
+            button2.TabIndex = 40;
+            button2.Text = "Wyczyść";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Highlight;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(12, 479);
+            button1.Name = "button1";
+            button1.Size = new Size(119, 50);
+            button1.TabIndex = 39;
+            button1.Text = "Wczytaj";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.HorizontalScrollbar = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(61, 92);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(852, 329);
+            listBox1.TabIndex = 38;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button7);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(textBox1);
+            panel2.Location = new Point(275, 221);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(448, 141);
+            panel2.TabIndex = 53;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Transparent;
+            button7.Image = (Image)resources.GetObject("button7.Image");
+            button7.Location = new Point(387, 16);
+            button7.Name = "button7";
+            button7.Size = new Size(40, 27);
+            button7.TabIndex = 41;
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.Highlight;
+            button3.Cursor = Cursors.Hand;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(76, 54);
+            button3.Name = "button3";
+            button3.Size = new Size(119, 50);
+            button3.TabIndex = 42;
+            button3.Text = "Zaloguj";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.Highlight;
+            button4.Cursor = Cursors.Hand;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(201, 54);
+            button4.Name = "button4";
+            button4.Size = new Size(133, 50);
+            button4.TabIndex = 45;
+            button4.Text = "Zmień hasło";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(19, 16);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Hasło";
+            textBox1.Size = new Size(362, 27);
+            textBox1.TabIndex = 43;
+            textBox1.UseSystemPasswordChar = true;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button8);
+            panel3.Controls.Add(button5);
+            panel3.Controls.Add(textBox4);
+            panel3.Controls.Add(button6);
+            panel3.Controls.Add(textBox2);
+            panel3.Controls.Add(textBox3);
+            panel3.Location = new Point(287, 178);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(401, 260);
+            panel3.TabIndex = 54;
+            panel3.Visible = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Transparent;
+            button8.Image = (Image)resources.GetObject("button8.Image");
+            button8.Location = new Point(340, 130);
+            button8.Name = "button8";
+            button8.Size = new Size(40, 27);
+            button8.TabIndex = 55;
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.Highlight;
+            button5.Cursor = Cursors.Hand;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.ForeColor = Color.White;
+            button5.Location = new Point(202, 130);
+            button5.Name = "button5";
+            button5.Size = new Size(119, 50);
+            button5.TabIndex = 56;
+            button5.Text = "Anuluj";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Clicked;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(18, 92);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "Powtórz nowe hasło";
+            textBox4.Size = new Size(362, 27);
+            textBox4.TabIndex = 53;
+            textBox4.UseSystemPasswordChar = true;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.Highlight;
+            button6.Cursor = Cursors.Hand;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button6.ForeColor = Color.White;
+            button6.Location = new Point(77, 130);
+            button6.Name = "button6";
+            button6.Size = new Size(119, 50);
+            button6.TabIndex = 55;
+            button6.Text = "Zmień";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Clicked;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(18, 16);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Aktualne hasło";
+            textBox2.Size = new Size(362, 27);
+            textBox2.TabIndex = 49;
+            textBox2.UseSystemPasswordChar = true;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(18, 54);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Nowe hasło";
+            textBox3.Size = new Size(362, 27);
+            textBox3.TabIndex = 51;
+            textBox3.UseSystemPasswordChar = true;
+            // 
+            // Form5
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(982, 553);
+            Controls.Add(panel2);
+            Controls.Add(panel3);
+            Controls.Add(panel1);
+            MaximizeBox = false;
+            Name = "Form5";
+            Text = "Form5";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button2;
+        private Button button1;
+        private ListBox listBox1;
+        private Panel panel2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox1;
+        private Panel panel3;
+        private Button button5;
+        private TextBox textBox4;
+        private Button button6;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Label label3;
+    }
+}
